666024f104a1ef405ca8b7cc042c3258c618469a
2pa-h8 <78879018+2pa-h8@users.noreply.github.com>
2021-03-28 18:57:51 +0300

    Add files via upload

---
+++ lab3/task5/task5_while/task5_while.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>;
+using namespace std;
+
+int i, k;
+double eps, s, res;
+
+int findFirstNegativeElement(double eps) {
+	i = 0;
+	s = pow(-1, i) * (pow(2, i) / (pow(i, i + 1) + 1));
+	while (true) {
+		i++;
+		s = pow(-1, i) * (pow(2, i) / (pow(i, i + 1) + 1));
+		k = i;
+		if (s > 0) continue;
+		if (abs(s) <= eps) {
+			return k;
+		}
+	}
+}
+
+void main(double eps) {
+	setlocale(LC_ALL, "RUS");
+	cout << "eps:";
+	cin >> eps;
+	res = findFirstNegativeElement(eps);
+	cout << "Первый отрицательный член последовательности:" << res << endl;
+}
